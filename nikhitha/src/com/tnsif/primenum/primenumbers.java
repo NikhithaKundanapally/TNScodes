0888ac9aa772fc59aefbbcd274d07eecb0f3b28a
Nikhitha <Nikhitha@SHIVANI>
2025-09-17 22:02:47 +0530

    6th commit

---
+++ nikhitha/src/com/tnsif/primenum/primenumbers.java
@@ -0,0 +1,14 @@
+package com.tnsif.primenum;
+
+public class primenumbers {
+
+	public static void main(String[] args) {
+		 int n =30, i, f = 0;
+	        if (n < 2) f = 1;
+	        for (i = 2; i <= n / 2; i++)
+	            if (n % i == 0) { f = 1; break; }
+	        System.out.println(f == 0 ? "Prime" : "Not Prime");
+
+	}
+
+}
