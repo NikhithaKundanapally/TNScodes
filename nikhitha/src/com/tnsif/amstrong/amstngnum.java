75f93bc6ba971c2f7c071239b2b301de03da62a8
Nikhitha <Nikhitha@SHIVANI>
2025-09-17 21:54:32 +0530

    4th commit

---
+++ nikhitha/src/com/tnsif/amstrong/amstngnum.java
@@ -0,0 +1,16 @@
+package com.tnsif.amstrong;
+
+public class amstngnum {
+
+	public static void main(String[] args) {
+		int n = 23, temp = n, sum = 0, d, len = String.valueOf(n).length();
+        while (temp > 0) {
+            d = temp % 10;
+            sum += Math.pow(d, len);
+            temp /= 10;
+        }
+        System.out.println(sum == n ? "Armstrong" : "Not Armstrong");
+
+	}
+
+}
