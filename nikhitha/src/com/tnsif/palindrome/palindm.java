79e44fea40ccd348c851dcf6ec9268d9c3061c50
Nikhitha <Nikhitha@SHIVANI>
2025-09-17 22:00:25 +0530

    5th commit

---
+++ nikhitha/src/com/tnsif/palindrome/palindm.java
@@ -0,0 +1,21 @@
+package com.tnsif.palindrome;
+
+public class palindm {
+	static boolean isPalindrome(int n) {
+        int rev = 0, temp = n;
+        while (temp > 0) {
+            rev = rev * 10 + temp % 10;
+            temp /= 10;
+        }
+        return n == rev;
+    }
+
+
+	public static void main(String[] args) {
+		 int n = 343;
+	        System.out.println(isPalindrome(n) ? "Palindrome" : "Not Palindrome");
+
+		
+	}
+
+}
