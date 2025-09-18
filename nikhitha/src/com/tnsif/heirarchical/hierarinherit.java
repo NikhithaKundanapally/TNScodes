ce873c1855855de1e703bfb1ff77306dc4c03ff9
Nikhitha <Nikhitha@SHIVANI>
2025-09-18 22:10:07 +0530

    commit 9

---
+++ nikhitha/src/com/tnsif/heirarchical/hierarinherit.java
@@ -0,0 +1,45 @@
+package com.tnsif.heirarchical;
+class Car3 {
+	 void start() {
+	     System.out.println("Car starts with a key.");
+	 }
+	}
+
+	class SportsCar extends Car3 {
+	 void turbo() {
+	     System.out.println("SportsCar has turbo speed.");
+	 }
+	}
+
+	class ElectricCar extends Car3 {
+	 void charge() {
+	     System.out.println("ElectricCar charges with electricity.");
+	 }
+	}
+
+	class LuxuryCar extends Car3 {
+	 void comfort() {
+	     System.out.println("LuxuryCar has high comfort features.");
+	 }
+	}
+
+
+public class hierarinherit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SportsCar sc = new SportsCar();
+	     sc.start();   
+	     sc.turbo();  
+
+	     ElectricCar ec = new ElectricCar();
+	     ec.start();   
+	     ec.charge();  
+
+	     LuxuryCar lc = new LuxuryCar();
+	     lc.start();   
+	     lc.comfort();
+
+	}
+
+}
