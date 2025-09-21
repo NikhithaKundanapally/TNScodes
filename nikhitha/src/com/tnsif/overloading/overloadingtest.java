3745097904d970a910f956d4dda2cdfada6aee58
Nikhitha <Nikhitha@SHIVANI>
2025-09-21 20:32:26 +0530

    commit 13

---
+++ nikhitha/src/com/tnsif/overloading/overloadingtest.java
@@ -0,0 +1,15 @@
+package com.tnsif.overloading;
+
+public class overloadingtest {
+	public void demo() {
+		System.out.println("this is default");
+		}
+		public void demo(int a) {
+			System.out.println("this is having one parameter");
+			
+		}
+		public void demo(int a,int b) {
+			System.out.println("this is value of a is:"+a+"the value of b  is:"+b);
+		}
+	}
+
---
+++ nikhitha/src/com/tnsif/overloading/overloadingtest1.java
@@ -0,0 +1,15 @@
+package com.tnsif.overloading;
+
+public class overloadingtest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		overloadingtest ov = new overloadingtest();
+		ov.demo();
+		ov.demo(20);
+		ov.demo(20,30);
+		
+}
+
+	}
+
